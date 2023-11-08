69a34eacc2cee9c3123c5509e8c553939c7ba27f
narendradzulqarnain <narendra.dz1107@gmail.com>
2023-11-08 16:23:50 +0700

    selesai tutorial 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/widgets/shop_card.dart';
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
   final List<ShopItem> items = [
@@ -22,7 +25,10 @@ class MyHomePage extends StatelessWidget {
         title: const Text(
           'Shopping List',
         ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
       ),
+      drawer: const LeftDrawer(),
       body: SingleChildScrollView(
         // Widget wrapper yang dapat discroll
         child: Padding(
@@ -65,12 +71,7 @@ class MyHomePage extends StatelessWidget {
 }
 
 
-class ShopItem {
-  final String name;
-  final IconData icon;
 
-  ShopItem(this.name, this.icon);
-}
 class ShopCard extends StatelessWidget {
   final ShopItem item;
 
@@ -82,12 +83,20 @@ class ShopCard extends StatelessWidget {
       color: Colors.indigo,
       child: InkWell(
         // Area responsive terhadap sentuhan
+        // Area responsif terhadap sentuhan
         onTap: () {
           // Memunculkan SnackBar ketika diklik
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
+
+          // Navigate ke route yang sesuai (tergantung jenis tombol)
+          if (item.name == "Tambah Produk") {
+
+            Navigator.push(context,MaterialPageRoute(builder: (context) => const ShopFormPage()));
+          }
+
         },
         child: Container(
           // Container untuk menyimpan Icon dan Text
